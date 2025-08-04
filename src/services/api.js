4f4b45f3313339c01ff934f7339f97f628267cbb
Gal Luski <109075193+galuski@users.noreply.github.com>
2025-08-04 21:33:20 +0300

    Moved API key to environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,5 @@
-// קובץ הגדרות עבור API של מזג אוויר
-
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "79c04de162bd11f2fe33977b6cecd440";
+export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-// קואורדינטות של בואנוס איירס
 export const lat = -34.6037;
 export const lon = -58.3816;
