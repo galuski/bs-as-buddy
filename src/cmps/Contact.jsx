4432ded947fe796eae77c50f5db93ec6e30fca5e
Gal Luski <109075193+galuski@users.noreply.github.com>
2025-08-06 22:52:14 +0300

    mail

--- src/cmps/Contact.jsx
+++ src/cmps/Contact.jsx
@@ -10,17 +10,20 @@ import StampVG from './../assets/icons/stamp.svg?react';
 import { Title } from "./Title";
 
 export function Contact() {
-          const { t } = useTranslation();
+    const { t } = useTranslation();
 
     return (
         <section className="contact">
             <Title title={t("Contacto")} />
             <div className="contact-container">
                 <div className="contact-list">
-                    <a href="https://www.instagram.com/buenosairesbuddy?igsh=MWQwdnJ3bjJmdXVraA=="><InstagramSVG className="contact-icon" /> Buenos Aires Buddy - Argentina</a>
+                    <a href="https://www.instagram.com/buenosairesbuddy?igsh=MWQwdnJ3bjJmdXVraA==" target="_blank"><InstagramSVG className="contact-icon" /> Buenos Aires Buddy - Argentina</a>
                     <a href="https://wa.me/5491159343218" target="_blank"><WhatsappSVG className="contact-icon" /> +54 9 11 5934-3218</a>
-                    <a href="https://www.facebook.com/buenosairesbuddy"><FacebookSVG className="contact-icon" /> Buenos Aires Buddy - Argentina</a>
-                    <a href="mailto:buenosairesbuddy@gmail.com"><MailSVG className="contact-icon" />buenosairesbuddy@gmail.com</a>
+                    <a href="https://www.facebook.com/buenosairesbuddy" target="_blank"><FacebookSVG className="contact-icon" /> Buenos Aires Buddy - Argentina</a>
+                    <a href="https://mail.google.com/mail/?view=cm&to=buenosairesbuddy@gmail.com" target="_blank">
+                        <MailSVG className="contact-icon" />
+                        buenosairesbuddy@gmail.com
+                    </a>
                 </div>
                 <div>
                     <StampVG className="stamp-icon" />
